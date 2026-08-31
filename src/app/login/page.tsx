00ad2dc88d540ab40ce0fcd9784f32bad66a36f6
Rizki Setya Pambudi <setyakiki76@gmail.com>
2026-08-31 22:17:46 +0700

    fix color login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 flex items-center justify-center p-4">
+    <div className="h-full  flex items-center justify-center p-4">
       <Toaster position="top-right" />
 
       <div className="w-full max-w-md bg-white dark:bg-gray-800 rounded-xl shadow-2xl p-8 border border-gray-200 dark:border-gray-700">
